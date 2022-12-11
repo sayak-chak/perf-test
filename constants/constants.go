9e0c3180c9905e35dcb400cf4012f213f5bef4a1
sayak-chak <44857480+sayak-chak@users.noreply.github.com>
2022-12-11 23:23:10 +0530

    Add constants package contatining supportted http methods and method types

---
+++ constants/constants.go
@@ -0,0 +1,11 @@
+package constants
+
+type httpMethod string
+type methodType string
+
+const GET httpMethod = "GET"
+const POST httpMethod = "POST"
+const PUT httpMethod = "POST"
+const PATCH httpMethod = "PATCH"
+
+const REST methodType = "REST"
